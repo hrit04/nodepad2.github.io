dd6b19ef42a9b525565bfba7a93338518c8541e0
Hritam Chowdhury <hritam.c04@gmail.com>
2021-08-13 21:13:48 +0530

    changes done

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p>This app is developed by Hritam Chwodhury to keep your plans saved without storing it into database</p>
+        </div>
+    )
+}
